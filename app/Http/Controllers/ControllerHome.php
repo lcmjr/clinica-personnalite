f014cdd2558a4c239303b644417d8979278785d7
Luiz Manhani <luiz.manhani@gmail.com>
2016-07-25 18:12:02 -0300

    Textos Home(Home text)

--- app/Http/Controllers/ControllerHome.php
+++ app/Http/Controllers/ControllerHome.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\ModelContato;
+use App\ModelTextos;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class ControllerHome extends Controller{
     public function pagina(){
+        $textos = ModelTextos::home();
         $dados_contato = ModelContato::dados_contato();
-        return view('home')->with('dados_contato',$dados_contato);
+        return view('home')->with('dados_contato',$dados_contato)->with("textos",$textos);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,22 @@
 
 @section('classe-pg', 'page-home')
 @section('conteudo')
+    <section class="section-home central row" id="home-section-1">
+        <h1 class="titulo cor-vinho col-md-4">{{$textos[1]->titulo}}</h1>
+        <div class="ctd-texto col-md-8"><?= $textos[1]->texto;?><br/><a href="/clinica" class="btn-reset">Conheça a clínica</a>
+        </div>
+    </section>
+    <section class="section-home seta-vinho fundo-vinho">
+        <div class="central">
+            <h1 class="titulo">{{$textos[0]->titulo}}</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="foto-borda">
+                        <img src="uploads/paginas/{{$textos[0]->foto}}" alt="{{$textos[0]->titulo}}"/>
+                    </div>
+                </div>
+                <div class="ctd-texto col-md-6"><?= $textos[0]->texto;?><br/><a href="/especializacoes" class="btn-reset">Saiba Mais</a></div>
+            </div>
+        </div>
+    </section>
 @stop
